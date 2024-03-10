3c6de35e5d84803b31b62a80f5c41ceed7a554bb
CansCe <seawoth187@gmail.com>
2024-03-10 18:49:25 +0700

    Update projecttile + simple pool + shooting

--- Assets/_game/Script/ProjectilesScript.cs
+++ Assets/_game/Script/ProjectilesScript.cs
@@ -5,9 +5,23 @@ using UnityEngine;
 
 public class ProjectilesScript : MonoBehaviour
 {
-    public float speed = 5f; 
+    public float speed = 5f;
+    public Rigidbody2D rb;
+    public float existanceTime = 2f;
+    private void Start()
+    {
+        rb.velocity = transform.forward * speed;
+    }
     void Update()
     {
-        
+        if(existanceTime <= 0)
+        {
+            gameObject.SetActive(false);
+            existanceTime = 2f;
+        }
+        else
+        {
+            existanceTime -= Time.deltaTime;
+        }
     }
 }
